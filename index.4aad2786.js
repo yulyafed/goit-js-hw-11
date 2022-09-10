0bf1c9dd36516e2249dc26ec94ed9a759cc3b378
yulyafed <99392460+yulyafed@users.noreply.github.com>
2022-09-10 16:48:11 +0000

    Deploying to gh-pages from @ yulyafed/goit-js-hw-11@2f5f88ca56fc9f3df93202bb678515cbc3dcbc8f 🚀

---
+++ index.4aad2786.js
@@ -0,0 +1,2 @@
+document.querySelector(".search-form").addEventListener("submit",(function(e){e.preventDefault(),console.log(e)}));
+//# sourceMappingURL=index.4aad2786.js.map
---
+++ index.9de67f25.js
@@ -0,0 +1,2 @@
+({galleryCardsForm:document.querySelector(".search-form")}).galleryCardsForm.addEventListener("submit",(function(e){e.preventDefault(),console.log(e)}));
+//# sourceMappingURL=index.9de67f25.js.map

f8c713e6306766aba29c466b6689ddbd084d3121
Yuliia Fedorychak <Yulyafed86@gmail.com>
2022-09-12 08:36:15 +0300

    add changes

--- src/galleryCardsApi.js
+++ src/galleryCardsApi.js
@@ -1,5 +1,23 @@
 import axios from "axios";
 
+export async function galleryCardsApi(name) {
+    try {
+        const KEY = '29840242-456192cfb7aa7dbb81a60ed73';
+        const BASE_URL = 'https://pixabay.com/api/';
+        let page = 1;
+        const per_page = 40;
+
+        const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
+        return response.data;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+
+
 // export function galleryCardsApi(item) {
 //     const KEY = '29840242-456192cfb7aa7dbb81a60ed73';
 //     const BASE_URL = 'https://pixabay.com/api/';
@@ -15,15 +33,3 @@ import axios from "axios";
 //             return response.json()
 //         });
 // }
- export async function galleryCardsApi(item) {
-     try {
-         const KEY = '29840242-456192cfb7aa7dbb81a60ed73';
-         const BASE_URL = 'https://pixabay.com/api/';
-         let page = 1;
-         const per_page = 40;
-         const response = await axios.get(`${BASE_URL}?key=${KEY}&q=${item}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${per_page}`);
-         return response.data;
-    } catch (error) {
-        console.error(error);
-    }
-}
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 import Notiflix from 'notiflix';
-import { galleryCardsApi } from "./galleryCardsApi"
+import { galleryCardsApi } from "./galleryCardsApi";
 
 const refs = {
      galleryCardsForm: document.querySelector('.search-form'),
      galleryCardsBox: document.querySelector('.gallery'),
 }
+
 refs.galleryCardsForm.addEventListener('submit', onGalleryCardsFormSubmit);
 
 async function onGalleryCardsFormSubmit(e) { 
-    e.preventDefault();
-     const targetItem = e.target.elements.searchQuery.value
+     e.preventDefault();
      
-     // galleryCardsApi(targetItem).then(data => console.log(data))
-     const response = await galleryCardsApi(targetItem)
+     const targetItem = e.target.elements.searchQuery.value;
+        
+     const response = await galleryCardsApi(targetItem);
+     console.log(response);
     
      if (response.total === 0) { 
           Notiflix.Notify.success('Sorry, there are no images matching your search query. Please try again.');
      }
 }
+
+
+
+     // galleryCardsApi(targetItem).then(data => console.log(data))
